9fa3911a5243709b1ac1b0675cad033e44f7bdf4
hadesyi <leesk55@gmail.com>
2018-10-20 07:50:57 +0900

    Implementing the Runnable Interface

--- src/Thread/MyThreadClass.java
+++ src/Thread/MyThreadClass.java
@@ -6,8 +6,17 @@ public class MyThreadClass extends Thread {
         System.out.println("Hello Java Thread!");
     }
 
+    @FunctionalInterface
+    public interface Runnable {
+        void run();
+    }
+
     public static void main(String[] args) {
         MyThreadClass myThread = new MyThreadClass();
         myThread.start();
+
+        Runnable  aRunnableObject = () ->  System.out.println("Hello Java  thread!");
+        Thread  myThread2 = new Thread(String.valueOf(aRunnableObject));
+        myThread2.start();
     }
 }
