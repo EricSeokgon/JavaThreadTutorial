3c26c17fc8de620513a8dfe4aa325053ff223870
hadesyi <leesk55@gmail.com>
2018-10-22 22:22:07 +0900

    The following code shows how to create a thread and print integers from 1 to 5 on the standard output.

---
+++ src/Thread/ThreadTest2.java
@@ -0,0 +1,13 @@
+package Thread;
+
+public class ThreadTest2 {
+    public static void main(String[] args) {
+        Thread t = new Thread(ThreadTest2::print);
+        t.start();
+    }
+    public static void print() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.print(i + "  ");
+        }
+    }
+}
