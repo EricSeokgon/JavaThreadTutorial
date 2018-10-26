375786e0fa999af13cad5f98f0cbf8a84eaf30ce
hadesyi <leesk55@gmail.com>
2018-10-27 00:21:28 +0900

    Synchronized keyword

---
+++ src/Thread/Synchronization.java
@@ -0,0 +1,11 @@
+package Thread;
+
+public class Synchronization {
+    public synchronized void someMethod_1() {
+        // Method code goes here
+    }
+
+    public static synchronized void someMethod_2() {
+        // Method code goes here
+    }
+}
