8f167be3fda61ca070a22f3df6e2b9ce13e607cd
hadesyi <leesk55@gmail.com>
2018-10-28 05:50:12 +0900

    Synchronized keyword

--- src/Thread/Synchronization.java
+++ src/Thread/Synchronization.java
@@ -2,10 +2,40 @@ package Thread;
 
 public class Synchronization {
     public synchronized void someMethod_1() {
-        // Method code goes here
+        // only one thread can execute here at a time
+    }
+
+    public void someMethod_11() {
+        synchronized (this) {
+            // only one thread can execute here at a time
+        }
+    }
+
+    public void someMethod_12() {
+        // multiple threads can execute here at a time
+        synchronized (this) {
+            // only one thread can execute here at a time
+        }
+        // multiple threads can execute here at a time
     }
 
     public static synchronized void someMethod_2() {
-        // Method code goes here
+        // only one thread can execute here at a time
+    }
+
+    public static void someMethod_21() {
+        synchronized (Synchronization.class) {
+            // only one thread can execute here at a time
+        }
+    }
+
+    public static void someMethod_22() {
+        // multiple threads can execute here at a time
+        synchronized (Synchronization.class) {
+            // only one thread can execute here at a time
+        }
+        // multiple threads can execute here at a time
     }
 }
+
+
