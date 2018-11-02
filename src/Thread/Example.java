959b37ed4ced8a0e9f51931946fe53c4afff578c
hadesyi <leesk55@gmail.com>
2018-11-03 01:37:31 +0900

    Synchronized keyword

---
+++ src/Thread/Example.java
@@ -0,0 +1,34 @@
+package Thread;
+
+public class Example {
+    private static int myValue = 1;
+    public static void main(String[] args) {
+        Thread t = new Thread(() -> {
+            while (true) {
+                updateBalance();
+            }
+        });
+        t.start();
+        t = new Thread(() -> {
+            while (true) {
+                monitorBalance();
+            }
+        });
+        t.start();
+    }
+
+    public static synchronized void updateBalance() {
+        System.out.println("start:" + myValue);
+        myValue = myValue + 1;
+        myValue = myValue - 1;
+        System.out.println("end:" + myValue);
+    }
+
+    public static synchronized void monitorBalance() {
+        int b = myValue;
+        if (b != 1) {
+            System.out.println("Balance  changed: " + b);
+            System.exit(1);
+        }
+    }
+}
