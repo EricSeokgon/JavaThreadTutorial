618f6e4c55322751f512f63ee00f67e5d7f9cbe4
hadesyi <leesk55@gmail.com>
2018-11-24 05:16:56 +0900

    Java Thread Sleep

---
+++ src/Thread/ThreadSleep.java
@@ -0,0 +1,15 @@
+package Thread;
+
+public class ThreadSleep {
+    public static void main(String[] args) {
+        try {
+            System.out.println("sleep for 5  seconds.");
+            Thread.sleep(5000);
+            // The "main" thread will sleep
+            System.out.println("woke up.");
+        } catch (InterruptedException e) {
+            System.out.println("interrupted.");
+        }
+        System.out.println("done.");
+    }
+}
