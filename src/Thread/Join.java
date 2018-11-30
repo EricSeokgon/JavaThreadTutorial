831c306ab4190ca55e2a8e77ded8fc57c824d3d2
hadesyi <leesk55@gmail.com>
2018-12-01 01:43:32 +0900

    Java Thread Join

---
+++ src/Thread/Join.java
@@ -0,0 +1,20 @@
+package Thread;
+
+public class Join {
+    public static void main(String[] args) {
+        Thread t1 = new Thread(Join::print);
+        t1.start();//from  ww  w .j  ava  2  s. c o  m
+        System.out.println("Done.");
+    }
+
+    public static void print() {
+        for (int i = 1; i <= 5; i++) {
+            try {
+                System.out.println("Counter: " + i);
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
