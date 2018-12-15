7c4a81d1b790ba2eb66a7a82e1232170d380d0f0
hadesyi <leesk55@gmail.com>
2018-12-16 04:36:27 +0900

    Java Thread Life Cycle

--- src/Thread/ThreadState.java
+++ src/Thread/ThreadState.java
@@ -4,9 +4,11 @@ class ThreadState extends Thread {
     private boolean keepRunning = true;
     private boolean wait = false;
     private Object syncObject = null;
+
     public ThreadState(Object syncObject) {
         this.syncObject = syncObject;
     }
+
     public void run() {
         while (keepRunning) {
             synchronized (syncObject) {
@@ -28,4 +30,33 @@ class ThreadState extends Thread {
     public void setWait(boolean wait) {
         this.wait = wait;
     }
+
+    public static void main(String[] args) throws InterruptedException {
+        Object syncObject = new Object();
+        ThreadState ts = new ThreadState(syncObject);
+        System.out.println("Before start()-ts.isAlive():" + ts.isAlive());
+        System.out.println("#1:" + ts.getState());
+
+        ts.start();
+        System.out.println("After start()-ts.isAlive():" + ts.isAlive());
+        System.out.println("#2:" + ts.getState());
+        ts.setWait(true);
+
+        Thread.currentThread().sleep(100);
+
+        synchronized (syncObject) {
+            System.out.println("#3:" + ts.getState());
+            ts.setWait(false);
+            syncObject.notifyAll();
+        }
+
+        Thread.currentThread().sleep(2000);
+        System.out.println("#4:" + ts.getState());
+        ts.setKeepRunning(false);
+
+        Thread.currentThread().sleep(2000);
+        System.out.println("#5:" + ts.getState());
+        System.out.println("At the   end. ts.isAlive():" + ts.isAlive());
+    }
+
 }
