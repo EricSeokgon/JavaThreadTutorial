8f5603a468d939a4392e13079795c0342884352b
hadesyi <leesk55@gmail.com>
2018-12-29 08:04:56 +0900

    Java Thread Interrupt

---
+++ src/Thread/Interrupt.java
@@ -0,0 +1,16 @@
+package Thread;
+
+public class Interrupt {
+    public static void main(String[] args) {
+        System.out.println("#1:" + Thread.interrupted());
+
+        // Now interrupt the main thread
+        Thread.currentThread().interrupt();
+
+        // Check if it has been interrupted
+        System.out.println("#2:" + Thread.interrupted());
+
+        // Check again if it has been interrupted
+        System.out.println("#3:" + Thread.interrupted());
+    }
+}
