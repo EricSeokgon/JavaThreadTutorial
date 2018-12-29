8101439bb1314c8595d16e977f243c3f1ac0b455
hadesyi <leesk55@gmail.com>
2018-12-30 05:19:02 +0900

    Java Thread Interrupt

---
+++ src/Thread/Interrupt2.java
@@ -0,0 +1,23 @@
+package Thread;
+
+public class Interrupt2 {
+    public static void main(String[] args) {
+        Thread t = new Thread(Interrupt2::run);
+        t.start();
+        try {
+            Thread.currentThread().sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        t.interrupt();
+    }
+
+    public static void run() {
+        int counter = 0;
+        while (!Thread.interrupted()) {
+            counter++;
+        }
+        System.out.println("Counter:" + counter);
+
+    }
+}
