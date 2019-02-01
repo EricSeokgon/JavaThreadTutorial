35fe71a4742f4cc26bd0b5b843031c5bd56fd109
hadesyi <leesk55@gmail.com>
2019-02-02 01:18:40 +0900

    Java Atomic Variables

---
+++ src/Thread_Concurrency_Packages/AtomicCounter.java
@@ -0,0 +1,11 @@
+package Thread_Concurrency_Packages;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+public class AtomicCounter {
+    private AtomicLong value = new AtomicLong(0L);
+
+    public long next() {
+        return value.incrementAndGet();
+    }
+}
